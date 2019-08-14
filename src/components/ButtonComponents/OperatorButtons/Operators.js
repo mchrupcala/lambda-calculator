43b04029cfb1e59413a43192e2d882fcd55e834c
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-08-13 21:34:00 -0400

    I was able to move the data from data.js into all the right wrapper & button components, and get them displaying onto app.js.

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
+import OperatorButton from "./OperatorButton";
 
 //Import your array data to from the provided data file
+import {operators} from "../../../data.js";
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  const [operatorState, setOperatorState] = useState(operators);
+  // console.log(operators);
+  // console.log(operatorState);
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component */}
+       {operators.map(item =>(
+         <OperatorButton  buttonValue={item.value}/>
+       ))}
+
     </div>
   );
 };
+
+export default Operators;
